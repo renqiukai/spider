5d239d547182051399befd1a7abc73378cbc48b0
renqiukai <renqiukai@qq.com>
2021-11-07 14:46:11 +0800

    fix bug

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,5 @@ export LANG=en_US.utf-8
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 pip3 install -r /home/fastapi-templates/requirements.txt -i https://pypi.douban.com/simple
 cd /home/spider/
-uvicorn app.main:app --host 0.0.0.0
+# uvicorn app.main:app --host 0.0.0.0
+/bin/bash
