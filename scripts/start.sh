2a9545a0990058d80ab6a5af29e981b1d176fc13
renqiukai <renqiukai@qq.com>
2021-11-07 14:43:02 +0800

    fix bug

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 crond
 crontab /home/spider/scripts/rqkscron
-export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
+export LC_ALL=en_US.utf-8
 python3 -m pip install --upgrade pip
 pip3 install -r /home/spider/requirements.txt -i https://pypi.douban.com/simple
 cd /home/spider/
