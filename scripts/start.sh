356173bb7b2d73b59abfbfd416ae797293930249
renqiukai <renqiukai@qq.com>
2021-11-07 14:44:59 +0800

    fix bug

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 crond
-crontab /home/spider/scripts/rqkscron
-export LANG=en_US.utf-8
+crontab /home/fastapi-templates/scripts/rqkscron
 export LC_ALL=en_US.utf-8
-python3 -m pip install --upgrade pip
-pip3 install -r /home/spider/requirements.txt -i https://pypi.douban.com/simple
+export LANG=en_US.utf-8
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+pip3 install -r /home/fastapi-templates/requirements.txt -i https://pypi.douban.com/simple
 cd /home/spider/
 uvicorn app.main:app --host 0.0.0.0
