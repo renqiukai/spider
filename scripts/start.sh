0b2cf173cd166bff93f1e9ab9b2275041d52b057
renqiukai <renqiukai@qq.com>
2021-11-07 14:38:55 +0800

    fix bug

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@ crond
 crontab /home/spider/scripts/rqkscron
 export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
+python3 -m pip install --upgrade pip
 pip3 install -r /home/spider/requirements.txt -i https://pypi.douban.com/simple
 cd /home/spider/
 uvicorn app.main:app --host 0.0.0.0
